5e05f8ac27455968afe4986ab75d1244da27cf10
senseImamuraY <iy5035704@gmail.com>
2022-09-19 06:57:21 +0900

    Initial commit

---
+++ Assets/AppleCatch/BasketController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController : MonoBehaviour
+{
+  public AudioClip appleSE;
+  public AudioClip bombSE;
+  AudioSource aud;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Application.targetFrameRate = 60;
+    this.aud = GetComponent<AudioSource>();
+  }
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.tag == "Apple")
+    {
+      this.aud.PlayOneShot(this.appleSE);
+    }
+    else
+    {
+      this.aud.PlayOneShot(this.bombSE);
+    }
+    Destroy(other.gameObject);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+      {
+        float x = Mathf.RoundToInt(hit.point.x);
+        float z = Mathf.RoundToInt(hit.point.z);
+        transform.position = new Vector3(x, 0, z);
+      }
+    }
+  }
+}
